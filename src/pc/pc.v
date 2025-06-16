4e5238ea468821f0771f30ccbd3758ca6f2d52e8
tylerli3 <tylerli3@illinois.edu>
2025-06-16 01:36:47 -0500

    Add files via upload

--- src/pc/pc.v
+++ src/pc/pc.v
@@ -2,19 +2,19 @@
 
 module pc(
 input	 			clk,
-input	 			reset,
-input				pc_write,
-input		[7:0]	next_pc,
-output	[7:0] pc_out
+input	 			reset,		// reset pc to 0
+input				pc_write,	// pc write enable
+input		[4:0]	next_pc,
+output	[4:0] pc_out
 );
 
-// 8 bit pc
-reg [7:0] pc_reg;
+// 5 bit pc
+reg [4:0] pc_reg;
 
 // sync update
 always @(posedge clk or posedge reset)begin
 	if(reset)
-		pc_reg <= 8'd0;
+		pc_reg <= 5'd0;
 	else if (pc_write)
 		pc_reg <= next_pc;
 end
--- src/pc/pc_tb.v
+++ src/pc/pc_tb.v
@@ -4,8 +4,8 @@ module pc_tb;
 reg			clk;
 reg			reset;
 reg			pc_write;
-reg	[7:0]	next_pc;
-wire	[7:0]	pc_out;
+reg	[4:0]	next_pc;
+wire	[4:0]	pc_out;
 
 //instantiate uut
 pc uut(
@@ -23,44 +23,44 @@ task rst(); begin
 end
 endtask
 
-always #5 clk = ~clk; // 10ns clock
+always #5 clk = ~clk; // 100MHz clock
 
 initial begin
 	//init
 	clk = 0;
 	reset = 0;
 	pc_write = 0;
-	next_pc = 8'd0;
+	next_pc = 5'd0;
 	
 	
 	$display("PC tb begin");
 	// case 1: test reset
 	rst();
-	if (pc_out !== 8'd0)
+	if (pc_out !== 5'd0)
 		$error("incorrect pc out after reset 1");
 	
 	// case 2: write to pc (pc+1)
 	rst();
 	pc_write = 1;
-	next_pc = 8'd1;
+	next_pc = 5'd1;
 	#10
-	if (pc_out !== 8'd1)
+	if (pc_out !== 5'd1)
 		$error("incorrect pc out after write 2");
 		
 	// case 3: attempt to write to pc without write enable
 	rst();
 	pc_write = 0;
-	next_pc = 8'd2;
+	next_pc = 5'd2;
 	#10
-	if (pc_out == 8'd2)
+	if (pc_out == 5'd2)
 		$error("pc out written to without write enable 3");
 	
 	// case 4: pc write jump
 	rst();
 	pc_write = 1;
-	next_pc = 8'd255;
+	next_pc = 5'd31;
 	#10
-	if (pc_out !== 8'd255)
+	if (pc_out !== 5'd31)
 		$error("incorrect pc out after jump write 4");
 	
 	$display("PC tb end");
